e9905c2aacf94b4abe1dd2c48f025f429467ad17
Romain <dereuxromain@gmail.com>
2020-12-10 16:23:06 +0100

    intial commit

---
+++ app/src/main/java/fr/utt/if26/romain_dereux/Test.java
@@ -0,0 +1,14 @@
+package fr.utt.if26.romain_dereux;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
