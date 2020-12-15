01d8db45da141b288b60321a3f1da707850c92c4
Romain <dereuxromain@gmail.com>
2020-12-15 18:32:01 +0100

    create a notification when the user click the fab button create cursus

--- app/src/main/java/fr/utt/if26/romain_dereux/ui/MainActivity.java
+++ app/src/main/java/fr/utt/if26/romain_dereux/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package fr.utt.if26.romain_dereux.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -11,18 +12,27 @@ import fr.utt.if26.romain_dereux.model.Cursus;
 import fr.utt.if26.romain_dereux.ui.adapter.CursusListAdapter;
 import fr.utt.if26.romain_dereux.viewmodel.CursusViewModel;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.nio.channels.Channels;
+
 public class MainActivity extends AppCompatActivity {
 
     private CursusViewModel cursusViewModel;
     public static final int NEW_CURSUS_ACTIVITY_REQUEST_CODE = 1;
     private ActivityMainBinding binding;
+    private static final String KEY_TEXT_CREATE = "key_text_create";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,9 +61,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, NewCursusActivity.class);
                 startActivityForResult(intent, NEW_CURSUS_ACTIVITY_REQUEST_CODE);
+                createNotification(intent);
+
             }
         });
 
+        createNotificationChannel();
 
     }
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -69,4 +82,40 @@ public class MainActivity extends AppCompatActivity {
                     Toast.LENGTH_LONG).show();
         }
     }
+
+    public void createNotification(Intent intent){
+        createNotificationChannel();
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "CHANNEL_ID")
+                .setSmallIcon(R.drawable.cursus)
+                .setContentTitle("Création cursus")
+                .setContentText("Création d'un nouveau cursus")
+                .setStyle(new NotificationCompat.BigTextStyle()
+                        .bigText("Création d'un nouveau cursus UTT"))
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
+
+        PendingIntent pi = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(pi);
+        mNotificationManager.notify(0, builder.build());
+
+    }
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "CHANNEL_NAME";
+            String description = "Channel intial pour les notifications";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel("CHANNEL_ID", name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
 }
