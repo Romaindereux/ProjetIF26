272b4a80f1c2d3517dfc2fe62102e923db6d90c2
Romain <dereuxromain@gmail.com>
2021-01-11 19:07:38 +0100

    bind branch to viewCursusActivity

--- app/src/main/java/fr/utt/if26/romain_dereux/ui/ViewCursusActivity.java
+++ app/src/main/java/fr/utt/if26/romain_dereux/ui/ViewCursusActivity.java
@@ -37,6 +37,79 @@ public class ViewCursusActivity extends AppCompatActivity {
 
     private Cursus cursus;
 
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_cursus);
+
+        /*  Toolbar */
+        ActionBar actionBar = getSupportActionBar();
+        cursus = (Cursus) getIntent().getSerializableExtra("cursus");
+        String title = getResources().getString(R.string.cursus);
+        actionBar.setTitle(title.concat(cursus.getIdentifier()));
+
+
+
+
+        ueViewModel = new ViewModelProvider(this).get(UEViewModel.class);
+        cursusViewModel = new ViewModelProvider(this).get(CursusViewModel.class);
+
+        bindListUE(cursus.getListCs(), binding.listCs, "CS");
+        bindListUE(cursus.getListTm(), binding.listTm, "TM");
+        bindListUE(cursus.getListMe(), binding.listMe, "ME");
+        bindListUE(cursus.getListHt(), binding.listHt, "HT");
+        bindListUE(cursus.getListEc(), binding.listEc, "EC");
+
+        binding.setSt09(cursus.isSt09());
+        binding.setSt10(cursus.isSt10());
+        binding.setNpml(cursus.isNpml());
+        binding.setBranche(cursus.getBranche());
+
+    }
+    public void renitializeSums(){
+        sumCs=0;
+        sumTm=0;
+        sumHt=0;
+        sumMe=0;
+        sumEc=0;
+    }
+
+    public void bindListUE(ArrayList<String> listString, ListView listToBind, String category) {
+        ListView listView = listToBind;
+        ArrayList<UE> listUE = new ArrayList<>();
+        Log.d(TAG, listString.toString());
+        renitializeSums();
+        for (String s : listString) {
+            Log.d(TAG, s);
+            UE ue = ueViewModel.getUEBySigle(s).get(0);
+            listUE.add(ue);
+            if (category == "CS") {
+                sumCs += ue.getCredit();
+                binding.setSumCs(String.valueOf(sumCs).concat(" / 24"));
+            } else if (category == "TM") {
+                sumTm += ue.getCredit();
+                binding.setSumTm(String.valueOf(sumTm).concat(" / 24"));
+            } else if (category == "ME") {
+                sumMe += ue.getCredit();
+                binding.setSumMe(String.valueOf(sumMe).concat(" / 4"));
+            } else if (category == "HT") {
+                sumHt += ue.getCredit();
+                binding.setSumHt(String.valueOf(sumHt).concat(" / 4"));
+            } else if (category == "EC") {
+                sumEc += ue.getCredit();
+                binding.setSumEc(String.valueOf(sumEc).concat(" / 12"));
+            }
+            binding.setSumCsTm(String.valueOf(sumCs + sumTm).concat(" / 84"));
+            binding.setSumMeHt(String.valueOf(sumMe + sumHt).concat(" / 16"));
+            binding.setSumTotal(String.valueOf(sumCs + sumTm + sumMe + sumHt + sumEc).concat(" / 180"));
+
+        }
+        Log.d(TAG, listUE.toString());
+        adapter = new UEListAdapter(this, listUE);
+        listView.setAdapter(adapter);
+        Utility.setListViewHeightBasedOnChildren(listToBind);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_toolbar_view_cursus, menu);
@@ -93,77 +166,5 @@ public class ViewCursusActivity extends AppCompatActivity {
                 break;
         }
 
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_view_cursus);
-
-        /*  Toolbar */
-        ActionBar actionBar = getSupportActionBar();
-        cursus = (Cursus) getIntent().getSerializableExtra("cursus");
-        String title = getResources().getString(R.string.cursus);
-        actionBar.setTitle(title.concat(cursus.getIdentifier()));
-
-
-
-
-        ueViewModel = new ViewModelProvider(this).get(UEViewModel.class);
-        cursusViewModel = new ViewModelProvider(this).get(CursusViewModel.class);
-
-        bindListUE(cursus.getListCs(), binding.listCs, "CS");
-        bindListUE(cursus.getListTm(), binding.listTm, "TM");
-        bindListUE(cursus.getListMe(), binding.listMe, "ME");
-        bindListUE(cursus.getListHt(), binding.listHt, "HT");
-        bindListUE(cursus.getListEc(), binding.listEc, "EC");
-
-        binding.setSt09(cursus.isSt09());
-        binding.setSt10(cursus.isSt10());
-        binding.setNpml(cursus.isNpml());
-
-    }
-    public void renitializeSums(){
-        sumCs=0;
-        sumTm=0;
-        sumHt=0;
-        sumMe=0;
-        sumEc=0;
-    }
-
-    public void bindListUE(ArrayList<String> listString, ListView listToBind, String category) {
-        ListView listView = listToBind;
-        ArrayList<UE> listUE = new ArrayList<>();
-        Log.d(TAG, listString.toString());
-        renitializeSums();
-        for (String s : listString) {
-            Log.d(TAG, s);
-            UE ue = ueViewModel.getUEBySigle(s).get(0);
-            listUE.add(ue);
-            if (category == "CS") {
-                sumCs += ue.getCredit();
-                binding.setSumCs(String.valueOf(sumCs).concat(" / 24"));
-            } else if (category == "TM") {
-                sumTm += ue.getCredit();
-                binding.setSumTm(String.valueOf(sumTm).concat(" / 24"));
-            } else if (category == "ME") {
-                sumMe += ue.getCredit();
-                binding.setSumMe(String.valueOf(sumMe).concat(" / 4"));
-            } else if (category == "HT") {
-                sumHt += ue.getCredit();
-                binding.setSumHt(String.valueOf(sumHt).concat(" / 4"));
-            } else if (category == "EC") {
-                sumEc += ue.getCredit();
-                binding.setSumEc(String.valueOf(sumEc).concat(" / 12"));
-            }
-            binding.setSumCsTm(String.valueOf(sumCs + sumTm).concat(" / 84"));
-            binding.setSumMeHt(String.valueOf(sumMe + sumHt).concat(" / 16"));
-            binding.setSumTotal(String.valueOf(sumCs + sumTm + sumMe + sumHt + sumEc).concat(" / 180"));
-
-        }
-        Log.d(TAG, listUE.toString());
-        adapter = new UEListAdapter(this, listUE);
-        listView.setAdapter(adapter);
-        Utility.setListViewHeightBasedOnChildren(listToBind);
     }
 }
